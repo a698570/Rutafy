e28378f7a9c5d593cb8d393bde296c9d09ab85d2
Miguel Escribano (miguescri) <miguescri@gmail.com>
2020-07-31 17:54:31 +0200

    Updates

---
+++ precache-manifest.afe48255c5935ef6080221b367718b30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dad4a464a8e06365ac2c3d10f17ee18",
+    "url": "/Rutafy/index.html"
+  },
+  {
+    "revision": "58884a6067dbd99efcf6",
+    "url": "/Rutafy/static/css/main.25ac248d.chunk.css"
+  },
+  {
+    "revision": "99914d6ca9385146dff2",
+    "url": "/Rutafy/static/js/2.512f490e.chunk.js"
+  },
+  {
+    "revision": "58884a6067dbd99efcf6",
+    "url": "/Rutafy/static/js/main.e68153d2.chunk.js"
+  },
+  {
+    "revision": "752d8c03dab78d8e34e7",
+    "url": "/Rutafy/static/js/runtime~main.43456458.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Rutafy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "208f3d38b50b2147d8c20291babed81a",
+    "url": "/Rutafy/static/media/shape.208f3d38.svg"
+  }
+]);
